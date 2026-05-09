49cb5d185f9e29712fb432942233eab43b230753
Mykhailo Illiashenko <michael.illiashenko@gmail.com>
2026-05-09 22:45:25 +0200

    init

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace VibrationWithDurationExample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace VibrationWithDurationExample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
